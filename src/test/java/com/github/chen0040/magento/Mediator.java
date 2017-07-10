ceeed7495e38844bbb5f82ea8c1d45c1b50d84e8
xschen <xs0040@gmail.com>
2017-07-10 18:16:28 +0800

    updated test account

--- src/test/java/com/github/chen0040/magento/Mediator.java
+++ src/test/java/com/github/chen0040/magento/Mediator.java
@@ -16,7 +16,7 @@ public class Mediator {
 
    public static final String url = "http://j-clef-web-01.japaneast.cloudapp.azure.com/";
    public static final String customerUsername = "xs0040@gmail.com";
-   public static final String customerPassword = "chen0040@";
+   public static final String customerPassword = "chen@0040";
    public static final String adminUsername = "admin";
-   public static final String adminPassword = "admin@2017";
+   public static final String adminPassword = "chen@0040";
 }
