b2fdd46c12fcec35523c43b5d7ce053d2b824353
xschen <xs0040@gmail.com>
2018-02-12 12:07:02 +0800

    update credential for testing

--- src/test/java/com/github/chen0040/magento/Mediator.java
+++ src/test/java/com/github/chen0040/magento/Mediator.java
@@ -6,17 +6,9 @@ package com.github.chen0040.magento;
  */
 public class Mediator {
 
-   /*
-   public static final String url = "http://furniture-e-commerce.box/";
-   public static final String customerUsername = "xs0040@gmail.com";
-   public static final String customerPassword = "chen0040@";
-   public static final String adminUsername = "admin";
-   public static final String adminPassword = "admin0040@";
-   */
-
-   public static final String url = "http://j-clef-web-01.japaneast.cloudapp.azure.com/";
-   public static final String customerUsername = "xs0040@gmail.com";
-   public static final String customerPassword = "chen@0040";
-   public static final String adminUsername = "admin";
-   public static final String adminPassword = "chen@0040";
+   public static final String url = "http://magento2.demo.ubertheme.com/";
+   public static final String customerUsername = "roni_cost@example.com";
+   public static final String customerPassword = "roni_cost@example.com";
+   public static final String adminUsername = "megamenu";
+   public static final String adminPassword = "megamenu@123";
 }
